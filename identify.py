a4b7aa3951067597e02c5cf3f265cf2eb3d90d53
senabayraktaroglu <senaaa.bayraktar@gmail.com>
2020-12-23 15:28:08 +0300

    FIX: if statement for unknown jetson

--- identify.py
+++ identify.py
@@ -33,10 +33,12 @@ def where_am_i():
             list_of_machine_ids =[3668 ,3448,2888,3310,3489,2180] 
             list_of_machine_names = ["NVIDIA Jetson Xavier NX", "NVIDIA Jetson Nano","NVIDIA Jetson AGX Xavier series","original NVIDIA Jetson TX2","NVIDIA® Jetson™ TX2i and Jetson TX2 4GB","Jetson TX1"]
             for i in range(len(list_of_machine_ids)):
+                found = False
                 if str(list_of_machine_ids[i]) in subprocess_return:
                     print(list_of_machine_names[i])
-                else:
-                    print("unknown jetson")     
+                    found = True
+            if found == False:
+                print("unknown jetson")     
 
         else:
             print('no idea')
