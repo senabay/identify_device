7e56c8a85b3dd1a28d779c65b5d3a626c3172c36
senabay <senabayraktaroglu@novit.ai>
2020-12-24 13:05:28 +0300

    fix unnecessary printing error

--- identify.py
+++ identify.py
@@ -18,11 +18,9 @@ def where_am_i():
         print("there is no file such as: /etc/os-release")    
 
     if current_machine == 'armv7l':
-        print('probably pi3')
-       
+        print('probably pi3')       
         subprocess_out = subprocess.Popen("cat /proc/cpuinfo", shell=True, stdout=subprocess.PIPE)
         subprocess_return = str(subprocess_out.stdout.read())
-        #print(str(subprocess_return))
         if 'Raspberry Pi 4' in subprocess_return:
             print("this is raspberry pi 4 ")
         if 'Raspberry Pi 3' in subprocess_return:  
@@ -34,6 +32,7 @@ def where_am_i():
             print('definitely jetson')            
             list_of_machine_ids =[2180,3310,3489,2888,3448,3668] 
             list_of_machine_names = ["Jetson TX1", "NVIDIA Jetson TX2","NVIDIA Jetson TX2i and Jetson TX2 4GB","NVIDIA Jetson AGX Xavier"," NVIDIA Jetson Nano","Jetson Xavier NX"]
+            
             def check_in_it(subprocess_that_made):
                 machine_is = None
                 for i in range(len(list_of_machine_ids)):
@@ -42,37 +41,36 @@ def where_am_i():
                         #print(machine_is)
                 return machine_is
             
-            machine_is = None
+            machine_is = None            
             
             if machine_is == None:
                 
-                subprocess_out = subprocess.Popen("cat /proc/device-tree/nvidia,boardids", shell=True, stdout=subprocess.PIPE)
-                subprocess_return1 = str(subprocess_out.stdout.read().decode("utf-8") )
+                subprocess_out = subprocess.Popen("cat /proc/device-tree/nvidia,boardids", shell=True, stdout=subprocess.PIPE,stderr = subprocess.PIPE)
+                subprocess_return1 = str(subprocess_out.stdout)
                 machine_is = check_in_it(subprocess_return1)
             
             if machine_is == None:
                
-                subprocess_out = subprocess.Popen("cat /proc/device-tree/compatible", shell=True, stdout=subprocess.PIPE)
-                subprocess_return1 = str(subprocess_out.stdout.read().decode("utf-8") )
+                subprocess_out = subprocess.Popen("cat /proc/device-tree/compatible", shell=True, stdout=subprocess.PIPE,stderr = subprocess.PIPE)
+                subprocess_return1 = str(subprocess_out.stdout)
                 machine_is = check_in_it(subprocess_return1)
                         
             if machine_is == None:
             
-                subprocess_out = subprocess.Popen("cat /proc/device-tree/nvidia,dtsfilename", shell=True, stdout=subprocess.PIPE)
-                subprocess_return1 = str(subprocess_out.stdout.read().decode("utf-8") )
-                machine_is = check_in_it(subprocess_return1)
-           
+                subprocess_out = subprocess.Popen("cat /proc/device-tree/nvidia,dtsfilename", shell=True, stdout=subprocess.PIPE,stderr = subprocess.PIPE)
+                subprocess_return1 = str(subprocess_out.stdout)
+                machine_is = check_in_it(subprocess_return1)            
             
             if machine_is == None:
-                print("unknown jetson") 
+                print("unknown jetson")     
             else:
                 print(machine_is)
-
 
         else:
             print('no idea')
     else:
         print('no idea')
+
 
 
 def main():
